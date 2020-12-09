44bb43ea559014c907f97ae5a070798695d9dabb
unknown <diebraga0055@gmail.com>
2020-12-09 14:42:07 +0000

    initial state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,22 @@
 import { NextPage } from 'next';
-import Link from 'next/link';
 import Layout from '../components/Layout';
 
-const IndexPage: NextPage = () => {
+import Cart from '../components/Cart';
+import CartSummary from '../components/CartSummary';
+import Products from '../components/Products';
+
+const DonatePage: NextPage = () => {
   return (
-    <Layout title="Home | Next.js + TypeScript Example">
-      <ul className="card-list">
-        <li>
-          <Link href="/donate-with-checkout">
-            <a className="card checkout-style-background">
-              <h2 className="bottom">Donate with Checkout</h2>
-              <img src="/checkout-one-time-payments.svg" />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/donate-with-elements">
-            <a className="card elements-style-background">
-              <h2 className="bottom">Donate with Elements</h2>
-              <img src="/elements-card-payment.svg" />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/use-shopping-cart">
-            <a className="card cart-style-background">
-              <h2 className="bottom">Use Shopping Cart</h2>
-              <img src="/use-shopping-cart.png" />
-            </a>
-          </Link>
-        </li>
-      </ul>
+    <Layout title="Shopping Cart | Next.js + TypeScript Example">
+      <div className="page-container">
+        <h1>Shopping Cart</h1>
+        <Cart>
+          <CartSummary />
+          <Products />
+        </Cart>
+      </div>
     </Layout>
   );
 };
 
-export default IndexPage;
+export default DonatePage;
